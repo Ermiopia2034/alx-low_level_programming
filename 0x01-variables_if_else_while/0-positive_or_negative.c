388d44842a83f231d78462c076c70149ae1c545f
Ermiopia2034 <ermiyas2034@gmail.com>
2022-09-09 15:57:37 -0400

    da

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,8 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	char n;
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	int n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	return (0);
 }
