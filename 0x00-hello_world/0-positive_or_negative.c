52d81ababd058a6f95d0eeec80a0a93c9f6258f9
Ermiopia2034 <ermiyas2034@gmail.com>
2022-09-09 15:39:07 -0400

    rty

---
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	if (n < 0)
+		printf("%d is negative\n");
+	else
+		if (n == 0)
+			printf("%d is zero\n");
+		else
+			printf("%d is possitve\n");
+	return (0);
+}
