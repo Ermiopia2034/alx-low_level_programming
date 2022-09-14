c137828f08f3736a3c3bab3a674ae7f12e5a0646
Ermiopia2034 <ermiyas2034@gmail.com>
2022-09-14 17:15:22 -0400

    case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - checks if a letter is lowercase or not
+ * Return; return (0); success.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
