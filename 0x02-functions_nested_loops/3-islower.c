6bcaaa25a16e8c32fe283b7744307f6735bbcd56
Ermiopia2034 <ermiyas2034@gmail.com>
2022-09-14 17:37:51 -0400

    case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _islower - checks if a letter is lowercase or not
  * Return; return (0); success.
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void print_alphabet_x10(void);
 void print_alphabet(void);
 int main(void);
 int _islower(int c);
+int _putchar(char c);
