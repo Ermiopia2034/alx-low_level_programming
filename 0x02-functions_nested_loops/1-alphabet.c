1c807b6b184b741f4ff3adbf4ce0a011ef474522
Ermiopia2034 <ermiyas2034@gmail.com>
2022-09-14 12:11:17 -0400

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet: prints the alphabet in lowercase
+ * Return: returns nothing.
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+}
