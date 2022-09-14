e9dd1ddfcb218f41a9372cc057c4db3122cb1f32
Ermiopia2034 <ermiyas2034@gmail.com>
2022-09-14 18:15:31 -0400

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - prints the sigh of a number
+ * @n: number being tested
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	return (0);
+
+}
