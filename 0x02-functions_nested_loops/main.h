09c7afbf28414b6df6f0acf5c8e6203e832b6510
Ermiopia2034 <ermiyas2034@gmail.com>
2023-06-21 17:16:59 +0300

    edited main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 
 void print_alphabet_x10(void);
 void print_alphabet(void);
@@ -15,4 +13,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-#endif /* MAIN_H */
